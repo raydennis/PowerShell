c2280945df6585033c9f98a24f4aa3fec3f64f89
raydennis <raydennis@outlook.com>
2021-05-13 11:22:19 -0600

    typo

--- AD/ReestablishTrust.ps1
+++ AD/ReestablishTrust.ps1
@@ -1,5 +1,5 @@
 ﻿$credential = Get-Credential 
 
-// old Reset-ComputerMachinePassword -Server it153cdc03.colleges.ad.unm.edu
+# old Reset-ComputerMachinePassword -Server it153cdc03.colleges.ad.unm.edu
 
 new Test-ComputerSecureChannel -Repair -Credential (Get-Credential)
